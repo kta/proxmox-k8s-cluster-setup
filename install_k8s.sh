434847ec5f128cc0852442f48df3cb27f040aa51
Sakakibara <h11404m@gmail.com>
2024-09-17 23:13:20 +0900

    k8s

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,23 +1,89 @@
 #!/usr/bin/env bash
+
 set -eu
-
-# pve-vm-cp-1
-KUBE_API_SERVER_VIP="192.168.100.201"
-EXTERNAL_KUBE_API_SERVER_NAME="pve-vm-cp-1"
-EXTERNAL_KUBE_API_SERVER_IP="192.168.100.201"
-
-# update時にインタラクティブに対応しない
-config_file="/etc/needrestart/needrestart.conf"
-new_value="'a'"
-sed -i "s/^#\$nrconf{restart} = 'i';/\$nrconf{restart} = $new_value;/" "$config_file"
-
+ 
+# example
+# chmod +x install_k8s.sh
+# ./install_k8s.sh pve-vm-cp-1 main
+# ./install_k8s.sh $(hostname) main
+#
+# special thanks!: 
+#       https://gist.github.com/inductor/32116c486095e5dde886b55ff6e568c8
+#       https://github.com/unchama/kube-cluster-on-proxmox/blob/main/scripts/k8s-node-setup.sh
 
 
-# apt update and upgrade
-apt update
-apt upgrade -y
 
-# add hosts
+# region : script-usage
+function usage() {
+    echo "usage> k8s-node-setup.sh [COMMAND]"
+    echo "[COMMAND]:"
+    echo "  help        show command usage"
+    echo "  pve-vm-cp-1    run setup script for pve-vm-cp-1"
+    echo "  pve-vm-cp-2    run setup script for pve-vm-cp-2"
+    echo "  pve-vm-cp-3    run setup script for pve-vm-cp-3"
+    echo "  pve-vm-wk-*    run setup script for pve-vm-wk-*"
+}
+
+
+case $1 in
+    pve-vm-cp-1|pve-vm-cp-2|pve-vm-cp-3|pve-vm-wk-*)
+        ;;
+    help)
+        usage
+        exit 255
+        ;;
+    *)
+        usage
+        exit 255
+        ;;
+esac
+
+# endregion
+
+# region : set variables
+
+# Set global variables
+TARGET_BRANCH=$2
+KUBE_API_SERVER_VIP=192.168.100.201
+VIP_INTERFACE=ens19
+NODE_IPS=( 192.168.100.201 192.168.100.202 192.168.100.203 )
+
+
+
+# set per-node variables
+case $1 in
+    pve-vm-cp-1)
+        KEEPALIVED_STATE=master
+        KEEPALIVED_PRIORITY=101
+        KEEPALIVED_UNICAST_SRC_IP=${NODE_IPS[0]}
+        KEEPALIVED_UNICAST_PEERS=( "${NODE_IPS[1]}" "${NODE_IPS[2]}" )
+        ;;
+    pve-vm-cp-2)
+        KEEPALIVED_STATE=BACKUP
+        KEEPALIVED_PRIORITY=99
+        KEEPALIVED_UNICAST_SRC_IP=${NODE_IPS[1]}
+        KEEPALIVED_UNICAST_PEERS=( "${NODE_IPS[0]}" "${NODE_IPS[2]}" )
+        ;;
+    pve-vm-cp-3)
+        KEEPALIVED_STATE=BACKUP
+        KEEPALIVED_PRIORITY=97
+        KEEPALIVED_UNICAST_SRC_IP=${NODE_IPS[2]}
+        KEEPALIVED_UNICAST_PEERS=( "${NODE_IPS[0]}" "${NODE_IPS[1]}" )
+        ;;
+    pve-vm-wk-*)
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+# endregion
+
+
+# region : setup for all-node
+
+
+# set hosts
 tee -a /etc/hosts <<EOS
 192.168.100.201 pve-vm-cp-1
 192.168.100.211 pve-vm-wk-1
@@ -27,11 +93,8 @@ tee -a /etc/hosts <<EOS
 192.168.100.213 pve-vm-wk-3
 EOS
 
-# disable swap
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-# ----------- install containerd ----------------
+# Install Containerd
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
@@ -40,11 +103,43 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-# sysctl設定
-cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+## Install containerd
+apt-get update && apt-get install -y apt-transport-https curl gnupg2
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get install -y containerd.io
+
+# Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+
+if grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
+echo "Config found, skip rewriting..."
+else
+sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
+fi
+
+sudo systemctl restart containerd
+
+# Modify kernel parameters for Kubernetes
+cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
 vm.overcommit_memory = 1
 vm.panic_on_oom = 0
 kernel.panic = 10
@@ -53,158 +148,143 @@ kernel.keys.root_maxkeys = 1000000
 kernel.keys.root_maxbytes = 25000000
 net.ipv4.conf.*.rp_filter = 0
 EOF
+sysctl --system
 
-# sysctlの反映
-sudo sysctl --system
-
-# install containerd
-# containerdのインストール
-
-sudo apt update
-sudo apt install -y containerd
-
-# containerdの設定
-# containerdの設定ファイルを作成
-sudo mkdir -p /etc/containerd
-sudo containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
-
-# containerdの設定ファイルを編集
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-
-# containerdの再起動
-sudo systemctl restart containerd
-
-# containerdの自動起動
-systemctl enable containerd
-
-# containerdのバージョン確認
-containerd --version
-
-# kubernetesのリポジトリ追加
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-tee /etc/apt/sources.list.d/kubernetes.list <<EOF
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-# kubernetesのインストール
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+# Install kubeadm
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# kubernetesの自動起動
-systemctl enable kubelet
+# Disable swap
+swapoff -a
 
-# kubernetesのバージョン確認
-kubeadm version
-
-cat >/etc/crictl.yaml <<EOF
+cat > /etc/crictl.yaml <<EOF
 runtime-endpoint: unix:///var/run/containerd/containerd.sock
 image-endpoint: unix:///var/run/containerd/containerd.sock
 timeout: 10
 EOF
 
-# install flannel
+# endregion
 
-# Ends except first-control-plane
+
+# Ends except worker-plane
 case $1 in
-pve-vm-cp-1) 
-  ;;
-pve-vm-cp-2 | pve-vm-cp-3)
-	exit 0
-	;;
-*)
-	exit 1
-	;;
+    pve-vm-wk-*)
+        exit 0
+        ;;
+    pve-vm-cp-1|pve-vm-cp-2|pve-vm-cp-3)
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+
+
+# region : setup for all-control-plane node
+
+# Pull images first
+kubeadm config images pull
+
+# endregion
+
+case $1 in
+    pve-vm-cp-1)
+        ;;
+    pve-vm-cp-2|pve-vm-cp-3)
+        exit 0
+        ;;
+    *)
+        exit 1
+        ;;
 esac
 
 # region : setup for first-control-plane node
 
-# Set init configuration for the first control plane
+# Set kubeadm bootstrap token using openssl
 KUBEADM_BOOTSTRAP_TOKEN=$(openssl rand -hex 3).$(openssl rand -hex 8)
 
-cat > "$HOME"/init_kubeadm.yaml <<EOF
-apiVersion: kubeadm.k8s.io/v1beta3
-kind: InitConfiguration
-bootstrapTokens:
-- token: "$KUBEADM_BOOTSTRAP_TOKEN"
-  description: "kubeadm bootstrap token"
-  ttl: "24h"
-nodeRegistration:
-  criSocket: "unix:///var/run/containerd/containerd.sock"
+
+# kubeadm init --apiserver-advertise-address 192.168.100.201  --pod-network-cidr 10.5.0.0/16 --upload-certs
+
+sudo kubeadm init \
+  --pod-network-cidr=10.244.0.0/16 \
+  --control-plane-endpoint=$KUBE_API_SERVER_VIP \
+  --apiserver-advertise-address=$KUBE_API_SERVER_VIP \
+  --upload-certs \
+  --token $KUBEADM_BOOTSTRAP_TOKEN
+
+mkdir -p "$HOME"/.kube
+cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+
+
+
+
+
+
+
+
+# region : setup for cluster
+# クラスタ初期セットアップ時に helm　をinstall
+
+# Install Helm CLI
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+# install CNI
+# Needs manual creation of namespace to avoid helm error
+kubectl create ns kube-flannel
+kubectl get ns
+kubectl label --overwrite ns kube-flannel pod-security.kubernetes.io/enforce=privileged
+
+helm repo add flannel https://flannel-io.github.io/flannel/
+helm install flannel --set podCidr="10.244.0.0/16" --namespace kube-flannel flannel/flannel
+
+
+# install MetalLB
+kubectl create ns metal-lb
+kubectl create ns wp
+kubectl get ns
+
+
+cat > "$HOME"/metallb-config.yaml <<EOF
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: default
+  namespace: metal-lb
+spec:
+  addresses:
+  - 192.168.100.200-192.168.100.244
+  autoAssign: true
 ---
-apiVersion: kubeadm.k8s.io/v1beta3
-kind: ClusterConfiguration
-networking:
-  serviceSubnet: "10.96.0.0/16"
-  podSubnet: "10.128.0.0/16"
-kubernetesVersion: "v1.29.2"
-controlPlaneEndpoint: "${KUBE_API_SERVER_VIP}:8443"
-apiServer:
-  certSANs:
-  - "${EXTERNAL_KUBE_API_SERVER_NAME}" # generate random FQDN to prevent malicious DoS attack
-  # - "${EXTERNAL_KUBE_API_SERVER_IP}" # generate random FQDN to prevent malicious DoS attack
-controllerManager:
-  extraArgs:
-    bind-address: "0.0.0.0"
-scheduler:
-  extraArgs:
-    bind-address: "0.0.0.0"
----
-apiVersion: kubelet.config.k8s.io/v1beta1
-kind: KubeletConfiguration
-cgroupDriver: "systemd"
-protectKernelDefaults: true
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: default
+  namespace: metal-lb
+spec:
+  ipAddressPools:
+  - default
 EOF
 
 
-# # kubernetesの設定
-# mkdir -p $HOME/.kube
-# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-# chown $(id -u):$(id -g) $HOME/.kube/config
-#
-# # kubernetesのネットワーク設定
-# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-#
-# # kubernetesのネットワーク確認
-# kubectl get pods --all-namespaces
-#
-# # kubernetesのノード確認
-# kubectl get nodes
-
-# kubeadm init --pod-network-cidr=10.244.0.0/16 --control-plane-endpoint=pve-vm-cp-1 --apiserver-cert-extra-sans=pve-vm-cp-1
-kubeadm init --config "$HOME"/init_kubeadm.yaml --skip-phases=addon/kube-proxy --ignore-preflight-errors=NumCPU,Mem
-
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# ----------------- Helm -----------------
-
-# Install Helm
-# https://helm.sh/docs/intro/install/#from-apt-debianubuntu
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
-sudo apt install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt update
-sudo apt install -y helm
-
-# Install MetalLB
-# https://metallb.universe.tf/installation/#installation-with-helm
 helm repo add metallb https://metallb.github.io/metallb
-kubectl create namespace metallb-system
-helm install metallb metallb/metallb -n metallb-system
+helm install metallb metallb/metallb -f metallb-config.yaml
 
-# ----------------- Preparation for connecting k8s nodes -----------------
+# endregion
+
+
 
 # Generate control plane certificate
 KUBEADM_UPLOADED_CERTS=$(kubeadm init phase upload-certs --upload-certs | tail -n 1)
 
 # Set join configuration for other control plane nodes
-cat >"$HOME"/join_kubeadm_cp.yaml <<EOF
+cat > "$HOME"/join_kubeadm_cp.yaml <<EOF
 apiVersion: kubelet.config.k8s.io/v1beta1
 kind: KubeletConfiguration
 cgroupDriver: "systemd"
@@ -216,7 +296,7 @@ nodeRegistration:
   criSocket: "unix:///var/run/containerd/containerd.sock"
 discovery:
   bootstrapToken:
-    apiServerEndpoint: "${KUBE_API_SERVER_VIP}:8443"
+    apiServerEndpoint: "${KUBE_API_SERVER_VIP}:6443"
     token: "$KUBEADM_BOOTSTRAP_TOKEN"
     unsafeSkipCAVerification: true
 controlPlane:
@@ -224,7 +304,9 @@ controlPlane:
 EOF
 
 # Set join configuration for worker nodes
-cat >"$HOME"/join_kubeadm_wk.yaml <<EOF
+# worker use command:    kubeadm join --config /root/join_kubeadm_wk.yaml
+#
+cat > "$HOME"/join_kubeadm_wk.yaml <<EOF
 apiVersion: kubelet.config.k8s.io/v1beta1
 kind: KubeletConfiguration
 cgroupDriver: "systemd"
@@ -236,23 +318,47 @@ nodeRegistration:
   criSocket: "unix:///var/run/containerd/containerd.sock"
 discovery:
   bootstrapToken:
-    apiServerEndpoint: "${KUBE_API_SERVER_VIP}:8443"
+    apiServerEndpoint: "${KUBE_API_SERVER_VIP}:6443"
     token: "$KUBEADM_BOOTSTRAP_TOKEN"
     unsafeSkipCAVerification: true
 EOF
 
-# install ansible
-sudo apt-get install -y ansible git sshpass
 
-# clone repo
-TARGET_BRANCH="main"
-git clone -b "${TARGET_BRANCH}" https://github.com/kta/proxmox-k8s-cluster-setup "$HOME"/kube-cluster-on-proxmox
 
-# export ansible.cfg target
-REPOSITORY_NAME=proxmox-k8s-cluster-setup
-ANSIBLE_CONFIG="$HOME"/${REPOSITORY_NAME}/ansible/ansible.cfg
+# region : setup for debug tool
 
-# run ansible-playbook
-ansible-galaxy role install -r "$HOME"/${REPOSITORY_NAME}/ansible/roles/requirements.yaml
-ansible-galaxy collection install -r "$HOME"/${REPOSITORY_NAME}/ansible/roles/requirements.yaml
-ansible-playbook -i "$HOME"/${REPOSITORY_NAME}/ansible/hosts/k8s-servers/inventory "$HOME"/${REPOSITORY_NAME}/ansible/site.yaml
+# install k9s
+wget https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_arm64.tar.gz -O - | tar -zxvf - k9s && sudo mv ./k9s /usr/local/bin/
+ 
+# endregion
+
+
+
+
+# =========================================================================
+# 
+# 
+# apt-get update && apt-get install -y apt-transport-https cGa-certificates curl gpg
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# 
+# # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# 
+# sudo apt-get update
+# sudo apt-get install -y kubelet kubeadm kubectl
+# sudo apt-mark hold kubelet kubeadm kubectl
+# 
+# sudo systemctl enable --now kubelet
+# 
+# 
+# 
+# 
+# echo $TARGET_BRANCH
+# echo $KUBE_API_SERVER_VIP
+# echo $VIP_INTERFACE
+# echo $NODE_IPS
+# echo $KEEPALIVED_STATE
+# echo $KEEPALIVED_PRIORITY
+# echo $KEEPALIVED_UNICAST_SRC_IP
+# echo $KEEPALIVED_UNICAST_PEERS
+
